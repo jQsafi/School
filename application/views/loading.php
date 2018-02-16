d3c6b599f00c863464312a1606e7396dfc299e9d
Safi Mohammad <amishafayat@gmail.com>
2018-02-17 01:25:04 +0600

    dddd

--- application/views/loading.php
+++
@@ -1,6 +0,0 @@
-<link href="<?=base_url()?>template/css/loading.css" type="text/css" rel="stylesheet">
-<div class="outer" id="loading">
-	<div class="inner" id="loading_message">
-	<?=$message?>
-	</div>
-</div>
--- application/views/progress_card/bottom.php
+++
@@ -1,7 +0,0 @@
-<table class="signature">
-	<tr>
-		<td><hr>Guardian Signature</td>
-		<td><hr>Class Teacher Signature</td>
-		<td><hr>Headmaster Signature</td>
-	</tr>
-</table>
